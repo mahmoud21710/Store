913bd8365a64eeb747ea09f3b319ddfaf43a7bc3
Mahmoud Abdel Fattah Ibrahim Abdel Razek <apdelfattahmahmoud1@gmail.com>
2024-11-23 22:01:41 +0200

    Done

---
+++ AdminDashBoard/Controllers/BrandController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.G04.Core;
+using Store.G04.Core.Entities;
+
+namespace AdminDashBoard.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BrandController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var Brands =await _unitOfWork.CreateRepository<ProductBrand,int>().GetAllAsync();
+
+
+            return View(Brands);
+        }
+        public async Task<IActionResult> Create(ProductBrand brand) 
+        {
+            try
+            {
+                await _unitOfWork.CreateRepository<ProductBrand, int>().AddAsync(brand);
+                await _unitOfWork.CompleteAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("Name", "Please Enter New Brand Name");
+                return View("Index",await _unitOfWork.CreateRepository<ProductBrand, int>().GetAllAsync());
+            }
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var brand = await _unitOfWork.CreateRepository<ProductBrand, int>().GetAsync(id);
+
+            _unitOfWork.CreateRepository<ProductBrand,int>().Delete(brand);
+            await _unitOfWork.CompleteAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AdminDashBoard/Controllers/TypeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.G04.Core;
+using Store.G04.Core.Entities;
+
+namespace AdminDashBoard.Controllers
+{
+    public class TypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var types =await  _unitOfWork.CreateRepository<ProductType , int>().GetAllAsync();
+            return View(types);
+        }
+
+        public async Task<IActionResult> Create(ProductType type) 
+        {
+            try
+            {
+                await _unitOfWork.CreateRepository<ProductType, int>().AddAsync(type);
+                await _unitOfWork.CompleteAsync();
+				return RedirectToAction("Index");
+			}
+			catch (Exception)
+			{
+				ModelState.AddModelError("Name", "Please Enter New Type Name");
+				return View("Index", await _unitOfWork.CreateRepository<ProductType, int>().GetAllAsync());
+			}
+		}
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var type = await _unitOfWork.CreateRepository<ProductType, int>().GetAsync(id);
+            _unitOfWork.CreateRepository<ProductType,int>().Delete(type);
+            await _unitOfWork.CompleteAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
